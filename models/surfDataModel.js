54724c283eb4e1bf72213982bd99ebceb234a6d2
Noah Hurley <noah.jhurley@gmail.com>
2024-06-07 14:02:41 -0700

    changed mongo model for surfData

--- models/surfDataModel.js
+++ models/surfDataModel.js
@@ -3,16 +3,15 @@ const { Schema } = mongoose;
 const Spot = require("./spotModel");
 
 const surfDataSchema = new Schema({
-    spot: { type: Schema.Types.ObjectId, ref: Spot, required: true },
-    date: { type: Date, required: true },
-    swellHeight: { type: Number, required: true },
-    swellPeriod: { type: Number, required: true },
-    swellDirection: { type: Number, required: true },
-    windSpeed: { type: Number, required: true },
-    windDirection: { type: Number, required: true },
-    tide: { type: String, required: true }
+  spot: { type: Schema.Types.ObjectId, ref: Spot, required: true },
+  date: { type: Date, required: true },
+  swellHeight: { type: Number, required: true },
+  swellPeriod: { type: Number, required: true },
+  swellDirection: { type: Number, required: true },
+  windSpeed: { type: Number, required: true },
+  windDirection: { type: Number, required: true },
+  tide: { type: String, required: true },
 });
 
-const SurfData = mongoose.model("SurfData", surfDataSchema);
-
+const SurfData = mongoose.model("SurfData", surfDataSchema, "surfDatas");
 module.exports = SurfData;
