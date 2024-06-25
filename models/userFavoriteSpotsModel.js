6ed155457f9d41bb50e099d214f649f5c5fa10b9
Noah Hurley <noah.jhurley@gmail.com>
2024-06-25 12:54:26 -0400

    fix favorite model

--- models/userFavoriteSpotsModel.js
+++ models/userFavoriteSpotsModel.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const User = require("./userModel");
+const Spot = require("./spotModel");
 
 const userFavoriteSpotsSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  spotIds: [{ type: Schema.Types.ObjectId, ref: "Spot", required: true }],
+  userId: { type: Schema.Types.ObjectId, ref: User, required: true },
+  spotIds: [{ type: Schema.Types.ObjectId, ref: Spot, required: true }],
 });
 
 const UserFavoriteSpots = mongoose.model(
