953abe2cedeeef9dfec419aa1ecdf829d8212156
Noah Hurley <noah.jhurley@gmail.com>
2024-06-25 12:49:23 -0400

    logging

--- controllers/userFavoriteSpotsController.js
+++ controllers/userFavoriteSpotsController.js
@@ -22,22 +22,26 @@ const addFavoriteSpot = async (req, res) => {
 };
 
 const getFavoriteSpots = async (req, res) => {
-  console.log("HERE!!");
-  console.log("Request:", req.body);
-  const { userId } = req.body;
-
   try {
-    const favorite = await UserFavoriteSpots.findOne({ userId }).populate(
-      "spot"
-    );
-    if (!favorite) {
-      return res.status(404).json({ error: "No favorite spots found" });
+    console.log("HERE!!");
+    console.log("Request:", req.body);
+    const { userId } = req.body;
+
+    try {
+      const favorite = await UserFavoriteSpots.findOne({ userId }).populate(
+        "spot"
+      );
+      if (!favorite) {
+        return res.status(404).json({ error: "No favorite spots found" });
+      }
+      console.log("Favorite spots:", favorite);
+      res.status(200).json({ favoriteSpots: favorite.spotIds });
+    } catch (err) {
+      console.error("Error:", err);
+      res.status(500).json({ error: err });
     }
-    console.log("Favorite spots:", favorite);
-    res.status(200).json({ favoriteSpots: favorite.spotIds });
   } catch (err) {
-    console.error("Error:", err);
-    res.status(500).json({ error: err });
+    console.log(err);
   }
 };
 
