9f1130947f520be05f1aac059ef80635a9d9df7a
Noah Hurley <noah.jhurley@gmail.com>
2024-07-07 12:28:59 -0700

    added protection to middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/userModel"); // Adjust the path according to your project structure
 const secret = "supersecretkey"; // Replace with your own secret key
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
@@ -11,6 +12,13 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, secret);
     console.log("decoded", decoded);
+
+    // Check if the user exists in the database
+    const user = await User.findById(decoded.id);
+    if (!user || user.email !== decoded.email) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
     req.userId = decoded.id;
     next();
   } catch (error) {
